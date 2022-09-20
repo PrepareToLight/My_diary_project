dbdfd5edb7a9b267a050d33d70d8f5bd4eecc41a
banaszek.letoslaw <banaszek.stanislaw@gmail.com>
2022-09-20 15:42:31 +0200

    created DataBase class to save entries in .db file

--- database.py
+++ database.py
@@ -1,4 +1,9 @@
 import datetime
+import sqlite3
+
+#common comands
+# CREATE TABLE IF NOT EXISTS <table_name> (val1 type, val2 type, ...);
+# INSERT INTO <table_name> (vals);
 
 entries = []
 
@@ -10,6 +15,39 @@ def add_entries(text):
 def view_entries():
     return entries
 
+class DataBase:
+    def __init__(self, title:str) -> None:
+        self.title = title
+        #if the file dosen't exists this command will create one
+        self.connection = sqlite3.connect(self.title + ".db")
+        #if it exists, than it will be just connected
+        self.tables = {}
+
+    def table_vals(self, dic_:dict) -> str:
+        vals = ""
+        for key in dic_:
+            vals += str(key) + " " + str(dic_[key]).upper() + ", "
+
+        return vals[:-2]    
+
+    def create_table(self, table_name, VALS:dict) -> None:
+        vals = self.table_vals(VALS)
+        with self.connection:
+            self.connection.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({vals})")
+        self.tables[table_name + " num_VALS"] = len(VALS)
+
+    def input_to_table(self, *INPUTS, table_name) -> None:
+        with self.connection:
+            self.connection.execute(f"INSERT INTO {table_name} VALUES {str(INPUTS)}")
 
 
-#print(datetime.datetime.now())
+    def close(self) -> None:
+        self.connection.close()
+
+
+#we connect the databse or creating it if we start program for the first time
+#and in the app.py file we close it on the last line
+my_dairy_databse = DataBase("MyDairy")
+my_dairy_databse.create_table("entries", {"date_time": "TEXT", "content": "TEXT"})
+
+
